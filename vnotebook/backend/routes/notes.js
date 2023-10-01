57cc563af334c9fe010d486b8bef5606610eb70e
sudarshana <sudarshanamgpv48@gmail.com>
2023-10-01 15:13:14 +0530

    vn6

--- vnotebook/backend/routes/notes.js
+++ vnotebook/backend/routes/notes.js
@@ -40,4 +40,53 @@ router.post("/addnote", fetchuser, [
     }
   }
 );
+
+// ROUTE 3: Updating a existing note using : PUT "/api/notes/updatenote" . Login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    const {title, description, tag} = req.body;
+    // create a newNote object
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+  
+    // Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){
+      return res.status(404).send("Not found");
+    }
+    if(note.user.toString() !== req.user.id){
+      return res.status(401).send("Not allowed");
+    }
+  
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+    res.json({note});
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).send("Internal Server Error");
+}
+})
+
+// ROUTE 4: Deleting a existing note using : DELETE "/api/notes/deletenote" . Login required
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+try {
+  // Find the note to be deleted and delete it
+  let note = await Note.findById(req.params.id);
+  if(!note){
+    return res.status(404).send("Not found");
+  }
+  // Allow deletion only if the user owns this note 
+  if(note.user.toString() !== req.user.id){
+    return res.status(401).send("Not allowed");
+  }
+
+  note = await Note.findByIdAndDelete(req.params.id)
+  res.json({"Success": "Deleted successfully", note: note});
+} catch (error) {
+  console.error(error.message);
+  res.status(400).send("Internal Server Error");
+}
+})
+
 module.exports = router;
